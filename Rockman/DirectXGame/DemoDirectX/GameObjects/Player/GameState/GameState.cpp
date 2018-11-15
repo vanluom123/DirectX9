44a814f37f00c07254ba74cda7499bd0cacacdd3
Phan Van Luom <phanvanluom97bd@gmail.com>
2018-11-15 11:33:37 +0700

    Added Rockman folder

---
+++ Rockman/DirectXGame/DemoDirectX/GameObjects/Player/GameState/GameState.cpp
@@ -0,0 +1,9 @@
+﻿#include "GameState.h"
+
+GameState::GameState(GamePlayer* gp) : gp(gp) { }
+
+void GameState::Update(float dt) { }
+
+void GameState::HandleKeyboard(std::map<int, bool> keys, float dt) { }
+
+void GameState::OnCollision(Entity::SideCollisions side) { }
